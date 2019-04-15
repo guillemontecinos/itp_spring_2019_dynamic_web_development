b979a01daf1fa4eb2308d07a0ef898e9078aa129
gamontec <gamontec@uc.cl>
2019-04-15 00:17:49 -0400

    part2b branch

--- week-2/assignment/partB/partB.js
+++ week-2/assignment/partB/partB.js
@@ -9,14 +9,20 @@ let app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
-    let band = req.query.band_name
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.post('/', function(req, res){
+    let band = req.body.band_name
     if(band === undefined){
-        res.sendFile(path.join(__dirname + '/index.html'))
+        res.send('The name you submitted is not correct.')
     }
     else{
-        res.send(band + ' is ' + insults[Math.floor(Math.random() * (insults.length))])
+        res.send(band + ' is ' + insults[Math.floor(insults.length * Math.random())])
     }
 })
+
+
 
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
